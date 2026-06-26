1f994610569b21bd3fa8c8368c6df4561577d47f
Pavlo Liapin <pavel.lyapin@gmail.com>
2026-06-26 12:49:05 -0500

    Update Angular to 22

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+
+// Flat-config equivalent of the previous .eslintrc.json, required by
+// ESLint 9 + @angular-eslint 22 (legacy eslintrc is no longer supported).
+module.exports = tseslint.config(
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...angular.configs.tsRecommended,
+    ],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/component-selector': [
+        'error',
+        { type: 'element', prefix: 'app', style: 'kebab-case' },
+      ],
+      '@angular-eslint/directive-selector': [
+        'error',
+        { type: 'attribute', prefix: 'app', style: 'camelCase' },
+      ],
+      quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [...angular.configs.templateRecommended],
+    rules: {},
+  },
+);
